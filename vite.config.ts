2bc0d80b6b44bacb75e7323a071b7a1d40bc9c29
kkk-kppi <banban980924@163.com>
2024-06-06 23:30:14 +0800

    vite.config.ts使用情景配置进行配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,52 +1,55 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import type { UserConfig, ConfigEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-// TODO: 修改成回调函数格式之后，报错：vitest.config.ts:6:3 - error TS2345: Argument of type 'UserConfigFnObject' is not assignable to parameter of type 'never'.
-export default defineConfig({
-  plugins: [vue(), vueJsx(), VueDevTools()],
-  resolve: {
-    // path alias setting, @ -> src/
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  cacheDir: '.vite', // change cache dir form node_modules/.vite to .vite
-  envDir: './env', // 指定环境变量文件在哪个位置
-  // css config
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-            @import "@/assets/scss/index.scss";
-          `
+export default defineConfig(({ mode }: ConfigEnv) => {
+  console.log('Vite Config Environment variable mode：', mode)
+  return {
+    plugins: [vue(), vueJsx(), VueDevTools()],
+    resolve: {
+      // path alias setting, @ -> src/
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-    }
-  },
-  // dev config
-  server: {
-    port: 51120,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:51121',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+    },
+    cacheDir: '.vite', // change cache dir form node_modules/.vite to .vite
+    envDir: './env', // 指定环境变量文件在哪个位置
+    // css config
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+              @import "@/assets/scss/index.scss";
+            `
+        }
       }
-    }
-  },
-  // preview config
-  preview: {
-    port: 51130,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:51131',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+    },
+    // dev config
+    server: {
+      port: 51120,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:51121',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    },
+    // preview config
+    preview: {
+      port: 51130,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:51131',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
   }
-})
+}) as UserConfig // 断言为UserConfig类型，否则报错：vitest.config.ts:6:3 - error TS2345: Argument of type 'UserConfigFnObject' is not assignable to parameter of type 'never'.
