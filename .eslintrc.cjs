0d3732a98f731e0c9eaaaa31fcf1c19a7ed23b28
kkk-kppi <banban980924@163.com>
2024-08-04 15:32:47 +0800

    config：更新eslintrc.cjs，项目忽略_开头的变量、参数和捕获错误

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
@@ -11,5 +11,15 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '^_', // 忽略以 `_` 开头的变量
+        argsIgnorePattern: '^_', // 忽略以 `_` 开头的函数参数
+        caughtErrorsIgnorePattern: '^_' // 忽略以 `_` 开头的捕获错误变量
+      }
+    ]
   }
 }
